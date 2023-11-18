27bd6983b7ba52a6ca55f20212e99b91829b66f7
Elaine Leiyoung <92842749+elaineleiyoung@users.noreply.github.com>
2023-11-18 17:28:36 -0500

    branch tab info

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("This is a test")
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button element by its ID
+    var scrapeButton = document.getElementById('scrapeButton');
+
+    // Add a click event listener to the button
+    scrapeButton.addEventListener('click', function() {
+        // Query the active tab in the current window
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            // Send a message to the content script in the active tab
+            console.log("found:")
+            console.log(tabs)
+            // chrome.tabs.sendMessage(tabs[0].id, {action: "scrapeCourses"}, function(response) {
+            //     // You can handle any response from the content script here
+            //     // For example, if your content script sends back scraped data or a confirmation message
+            //     console.log('Response from content script:', response);
+            // });
+        });
+    }, false);
+}, false);
+console.log("hello world")
