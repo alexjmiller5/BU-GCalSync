327185f285e0b850569ffbe931167e15681ca1ca
Alex Miller <amiller06880@gmail.com>
2023-11-18 17:44:31 -0500

    Initial version of the application with OAuth2 authentication and Google Calendar API integration.

---
+++ popup/oauth.js
@@ -0,0 +1,13 @@
+document.getElementById('google-sign-in-btn').addEventListener('click', function() {
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+      if (chrome.runtime.lastError) {
+        document.getElementById('status-message').textContent = 'Sign in failed. Please try again.';
+        console.log(chrome.runtime.lastError);
+      } else {
+        // Use the token for your API calls; we'll validate the token in the background script
+        document.getElementById('status-message').textContent = 'Sign in successful!';
+        // You could call a function here that uses the token to interact with the Google Calendar API.
+        console.log(token);
+      }
+    });
+  });
