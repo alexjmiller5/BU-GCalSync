693cf1aff08d0d3b497b1731ad26f05bcb9be18b
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2023-11-18 18:35:47 -0500

    fixed routing

--- popup/oauth.js
+++ popup/oauth.js
@@ -8,6 +8,13 @@ document.getElementById('google-sign-in-btn').addEventListener('click', function
         document.getElementById('status-message').textContent = 'Sign in successful!';
         // You could call a function here that uses the token to interact with the Google Calendar API.
         console.log(token);
+
+        // Dynamically load and display the "parser.html" content in the popup
+        fetch(chrome.runtime.getURL('popup/parser.html'))
+        .then(response => response.text())
+        .then(html => {
+          document.documentElement.innerHTML = html;
+        });
       }
     });
   });
