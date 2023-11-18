17436a38852aba040cba2a1b2642ea51d0166fcc
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2023-11-18 18:25:45 -0500

    route to different html after oauth

--- popup/oauth.js
+++ popup/oauth.js
@@ -7,7 +7,14 @@ document.getElementById('google-sign-in-btn').addEventListener('click', function
         // Use the token for your API calls; we'll validate the token in the background script
         document.getElementById('status-message').textContent = 'Sign in successful!';
         // You could call a function here that uses the token to interact with the Google Calendar API.
-        console.log(token);
+        
+        // Dynamically load and display the "parser.html" content in the popup
+        fetch(chrome.runtime.getURL('popup/parser.html'))
+        .then(response => response.text())
+        .then(html => {
+          document.documentElement.innerHTML = html;
+        });
+
       }
     });
   });
