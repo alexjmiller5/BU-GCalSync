9bf26b7d59cd5126ba7bcd3160ac79d26f1ad5c0
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2023-11-19 02:15:04 -0500

    added condtional coloring to icon

--- background.js
+++ background.js
@@ -1,27 +1,47 @@
 // Implement the service worker to check if the user is already authenticated on opening the extension
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener((activeInfo) => {
     // Perform initial setup and check for an existing token
     chrome.identity.getAuthToken({ interactive: false }, function (token) {
         if (chrome.runtime.lastError || !token) {
             // Not authenticated or an error occurred, set popup to OAuth
             chrome.action.setPopup({ popup: 'popup/popup.html' });
         } else {
-            // Authenticated, set popup to parser
-            chrome.action.setPopup({ popup: 'popup/parser.html' });
+            // otherwise 
+            chrome.tabs.get(activeInfo.tabId, function(tab) {
+                if (tab.url && tab.url.includes("ModuleName=allsched.pl")) {
+                    console.log(tab.url, "is correct");
+                    // Set popup to parser
+                    chrome.action.setPopup({ popup: 'popup/parser.html' });
+                    // Set icon to calendar-modified when URL is correct
+                    chrome.action.setIcon({ path: "images/calendar.png", tabId: activeInfo.tabId })
+                } else {
+                    console.log(tab.url, "is not correct");
+                    // Set popup to default
+                    chrome.action.setPopup({ popup: 'popup/studentLink.html' });
+                    // Set icon to calendar-modified when URL is correct
+                    chrome.action.setIcon({ path: "images/calendar-modified.png", tabId: activeInfo.tabId })
+                }
+            });
+            // // Authenticated, set popup to parser
+            // chrome.action.setPopup({ popup: 'popup/parser.html' });
         }
     });
 });
 
 chrome.tabs.onActivated.addListener(activeInfo => {
     chrome.tabs.get(activeInfo.tabId, function(tab) {
-        if (tab.url && tab.url.includes("link/bin/uiscgi_studentlink.pl")) {
+        if (tab.url && tab.url.includes("ModuleName=allsched.pl")) {
             console.log(tab.url, "is correct");
             // Set popup to parser
             chrome.action.setPopup({ popup: 'popup/parser.html' });
+            // Set icon to calendar-modified when URL is correct
+            chrome.action.setIcon({ path: "images/calendar.png", tabId: activeInfo.tabId })
         } else {
             console.log(tab.url, "is not correct");
             // Set popup to default
             chrome.action.setPopup({ popup: 'popup/studentLink.html' });
+            // Set icon to calendar-modified when URL is correct
+            chrome.action.setIcon({ path: "images/calendar-modified.png", tabId: activeInfo.tabId })
         }
     });
 });
