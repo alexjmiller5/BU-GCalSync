8333815733bfede4996d9fa94f083cef10a6df00
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2023-11-19 02:50:59 -0500

    fixed login functionality

--- background.js
+++ background.js
@@ -1,67 +1,43 @@
 // Implement the service worker to check if the user is already authenticated on opening the extension
-chrome.runtime.onInstalled.addListener((activeInfo) => {
+chrome.runtime.onInstalled.addListener(() => {
     // Perform initial setup and check for an existing token
     chrome.identity.getAuthToken({ interactive: false }, function (token) {
         if (chrome.runtime.lastError || !token) {
             // Not authenticated or an error occurred, set popup to OAuth
             chrome.action.setPopup({ popup: 'popup/popup.html' });
         } else {
-            // otherwise 
-            chrome.tabs.get(activeInfo.tabId, function(tab) {
-                if (tab.url && tab.url.includes("ModuleName=allsched.pl")) {
-                    console.log(tab.url, "is correct");
-                    // Set popup to parser
-                    chrome.action.setPopup({ popup: 'popup/parser.html' });
-                    // Set icon to calendar-modified when URL is correct
-                    chrome.action.setIcon({ path: "images/calendar.png", tabId: activeInfo.tabId })
-                } else {
-                    console.log(tab.url, "is not correct");
-                    // Set popup to default
-                    chrome.action.setPopup({ popup: 'popup/studentLink.html' });
-                    // Set icon to calendar-modified when URL is correct
-                    chrome.action.setIcon({ path: "images/calendar-modified.png", tabId: activeInfo.tabId })
-                }
-            });
-            // // Authenticated, set popup to parser
-            // chrome.action.setPopup({ popup: 'popup/parser.html' });
+            // Authenticated, set popup to parser
+            chrome.action.setPopup({ popup: 'popup/studentLink.html' });
         }
     });
 });
+
+// Function to update the icon and popup based on URL
+function updateIconAndPopup(tabId, url) {
+    if (url && url.includes("ModuleName=allsched.pl")) {
+        console.log(url, "is correct");
+        // Set icon to calendar-modified when URL is correct
+        chrome.action.setIcon({ path: "images/calendar.png", tabId: tabId });
+        // Set popup to parser
+        chrome.action.setPopup({ popup: 'popup/parser.html' });
+    } else {
+        console.log(url, "is not correct");
+        // Set icon to default when URL is not correct
+        chrome.action.setIcon({ path: "images/calendar-modified.png", tabId: tabId });
+        // Set popup to default
+        chrome.action.setPopup({ popup: 'popup/studentLink.html' });
+    }
+}
 
 chrome.tabs.onActivated.addListener(activeInfo => {
     chrome.tabs.get(activeInfo.tabId, function(tab) {
-        if (tab.url && tab.url.includes("ModuleName=allsched.pl")) {
-            console.log(tab.url, "is correct");
-            // Set popup to parser
-            chrome.action.setPopup({ popup: 'popup/parser.html' });
-            // Set icon to calendar-modified when URL is correct
-            chrome.action.setIcon({ path: "images/calendar.png", tabId: activeInfo.tabId })
-        } else {
-            console.log(tab.url, "is not correct");
-            // Set popup to default
-            chrome.action.setPopup({ popup: 'popup/studentLink.html' });
-            // Set icon to calendar-modified when URL is correct
-            chrome.action.setIcon({ path: "images/calendar-modified.png", tabId: activeInfo.tabId })
-        }
+        updateIconAndPopup(activeInfo.tabId, tab.url);
     });
 });
 
-
-// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-//     if (changeInfo.status === 'complete') {
-//         checkTabAndRunScript(tabId, tab.url);
-//     }
-// });
-
-// chrome.tabs.onActivated.addListener(function (activeInfo) {
-//     chrome.tabs.get(activeInfo.tabId, function (tab) {
-//         checkTabAndRunScript(activeInfo.tabId, tab.url);
-//     });
-// });
-
-// function checkTabAndRunScript(tabId, url) {
-//     // Check if the tab matches the conditions you want
-//     if (url.includes("theConditionYouWantToCheck")) {
-//         console.log('Tab is the studentlink site')
-//     }
-// }
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    // Check if the tab URL is updated
+    if (changeInfo.url) {
+        updateIconAndPopup(tabId, changeInfo.url);
+    }
+});
